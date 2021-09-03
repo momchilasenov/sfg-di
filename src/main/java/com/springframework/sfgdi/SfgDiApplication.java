4ad9a8ff8915141b757109a7234cda6750ebff94
Momchil Asenov <momchil276@gmail.com>
2021-09-03 13:59:46 +0300

    Dependency Injection demo, initial commit

---
+++ src/main/java/com/springframework/sfgdi/SfgDiApplication.java
@@ -0,0 +1,42 @@
+package com.springframework.sfgdi;
+
+import com.springframework.sfgdi.controllers.*;
+import com.springframework.sfgdi.services.ConstructorGreetingService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SfgDiApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+
+		I18nController i18nController = (I18nController) ctx.getBean("i18nController");
+		System.out.println(i18nController.sayHello());
+
+		MyController myController = (MyController) ctx.getBean("myController");
+
+		System.out.println(myController.sayHello());
+
+		System.out.println("------------------------------");
+
+		System.out.print("Property ");
+		PropertyInjectedController propertyInjectedController = (PropertyInjectedController)
+				ctx.getBean("propertyInjectedController");
+		System.out.println(propertyInjectedController.getGreeting());
+
+		System.out.print("Setter ");
+		SetterInjectedController setterInjectedController = (SetterInjectedController)
+				ctx.getBean("setterInjectedController");
+		System.out.println(setterInjectedController.getGreeting());
+
+		System.out.print("Constructor ");
+		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController)
+				ctx.getBean("constructorInjectedController");
+		System.out.println(constructorInjectedController.getGreeting());
+
+
+	}
+
+}
---
+++ src/main/java/com/springframework/sfgdi/controllers/ConstructorInjectedController.java
@@ -0,0 +1,22 @@
+package com.springframework.sfgdi.controllers;
+
+import com.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ConstructorInjectedController
+{
+  private final GreetingService greetingService;
+
+  public ConstructorInjectedController(@Qualifier("constructorGreetingService") GreetingService greetingService)
+  {
+    this.greetingService = greetingService;
+  }
+
+  public String getGreeting()
+  {
+    return greetingService.sayGreeting();
+  }
+}
