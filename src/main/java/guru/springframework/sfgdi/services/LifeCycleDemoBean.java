d4ec9c5ffe397a31f318f7adeb188b7e0d042162
Momchil Asenov <momchil276@gmail.com>
2021-09-10 09:55:06 +0300

    Add configuration class to create beans

--- src/main/java/guru/springframework/sfgdi/services/LifeCycleDemoBean.java
+++ src/main/java/guru/springframework/sfgdi/services/LifeCycleDemoBean.java
@@ -11,56 +11,65 @@ import javax.annotation.PreDestroy;
 
 @Component
 public class LifeCycleDemoBean implements InitializingBean, DisposableBean, BeanNameAware,
-        BeanFactoryAware, ApplicationContextAware {
+    BeanFactoryAware, ApplicationContextAware
+{
+  public LifeCycleDemoBean()
+  {
+    System.out.println("## I'm in the LifeCycleBean Constructor");
+  }
 
+  @Override
+  public void destroy() throws Exception
+  {
+    System.out.println("## The Lifecycle bean has been terminated");
 
-    public LifeCycleDemoBean() {
-        System.out.println("## I'm in the LifeCycleBean Constructor");
-    }
+  }
 
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("## The Lifecycle bean has been terminated");
+  @Override
+  public void afterPropertiesSet() throws Exception
+  {
+    System.out.println("## The LifeCycleBean has its properties set!");
 
-    }
+  }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("## The LifeCycleBean has its properties set!");
+  @Override
+  public void setBeanFactory(BeanFactory beanFactory) throws BeansException
+  {
+    System.out.println("## Bean Factory has been set");
+  }
 
-    }
+  @Override
+  public void setBeanName(String name)
+  {
+    System.out.println("## My Bean Name is: " + name);
 
-    @Override
-    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
-        System.out.println("## Bean Factory has been set");
-    }
+  }
 
-    @Override
-    public void setBeanName(String name) {
-        System.out.println("## My Bean Name is: " + name);
+  @Override
+  public void setApplicationContext(ApplicationContext applicationContext) throws BeansException
+  {
+    System.out.println("## Application context has been set");
+  }
 
-    }
+  @PostConstruct
+  public void postConstruct()
+  {
+    System.out.println("## The Post Construct annotated method has been called");
+  }
 
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        System.out.println("## Application context has been set");
-    }
+  @PreDestroy
+  public void preDestroy()
+  {
+    System.out.println("## The Predestroy annotated method has been called");
+  }
 
-    @PostConstruct
-    public void postConstruct(){
-        System.out.println("## The Post Construct annotated method has been called");
-    }
+  public void beforeInit()
+  {
+    System.out.println("## - Before Init - Called by Bean Post Processor");
+  }
 
-    @PreDestroy
-    public void preDestroy() {
-        System.out.println("## The Predestroy annotated method has been called");
-    }
-
-    public void beforeInit(){
-        System.out.println("## - Before Init - Called by Bean Post Processor");
-    }
-
-    public void afterInit(){
-        System.out.println("## - After init called by Bean Post Processor");
-    }
+  public void afterInit()
+  {
+    System.out.println("## - After init called by Bean Post Processor");
+  }
 }
